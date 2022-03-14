3da26d3b613250e053a7943787d5d990a462f7df
MaxWang <wmaxtime@gmail.com>
2022-03-14 18:15:13 +0800

    update sunburst

--- 5sunburstClass.js
+++ 5sunburstClass.js
@@ -1,7 +1,7 @@
 class Sunburst {
   constructor(ds, args) {
     const def = {
-      el: '#chart_svg2',
+      el: '#chart_svg',
       title: 'Pie Chart',
       marginTop: 50,
       marginBottom: 30,
@@ -46,21 +46,17 @@ class Sunburst {
       .attr('transform', `translate(${ml},${mt})`)
     vis.width -= vis.marginX
     vis.height -= vis.marginY
-    vis.radius = Math.min(width, height) / 6
+    vis.radius = d3.min([width, height]) / 2
     vis.createArc()
     vis.view = view
   }
   wrangleData(ds) {
     const root = this.generator(ds)
-    root.each(d => (d.current = d))
 
     this.updateVis(root)
   }
   generator(ds) {
-    const root = d3
-      .hierarchy(ds)
-      .sum(d => d.value)
-      .sort((a, b) => b.value - a.value)
+    const root = d3.hierarchy(randomData).sum(d => d.value)
     return d3.partition().size([2 * Math.PI, root.height + 1])(root)
   }
   createArc() {
@@ -69,121 +65,36 @@ class Sunburst {
       .arc()
       .startAngle(d => d.x0)
       .endAngle(d => d.x1)
-      .padAngle(d => Math.min((d.x1 - d.x0) / 2, 0.005))
-      .padRadius(vis.radius * 1.5)
-      .innerRadius(d => d.y0 * vis.radius)
-      .outerRadius(d => Math.max(d.y0 * vis.radius, d.y1 * vis.radius - 1))
-
-    vis.color = d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, 27))
-  }
-  arcVisible(d) {
-    return d.y1 <= 3 && d.y0 >= 1 && d.x1 > d.x0
+      .innerRadius(d => (d.y0 * vis.radius) / 3)
+      .outerRadius(d => {
+        return Math.max(d.y0 * vis.radius, d.y1 * vis.radius - 10) / 3
+      })
+    d3.schemePastel1.unshift('transparent')
+    vis.color = d3
+      .scaleOrdinal()
+      .domain([undefined, 'A', 'B', 'C', 'D', 'E'])
+      .range(d3.schemePastel1)
   }
 
   updateVis(datum) {
     const vis = this
-    vis.groups = vis.view
+
+    vis.view
       .append('g')
-      .attr('transform', `translate(${d3.map([this.width, this.height], b => b / 2)})`)
-    // console.log(datum)
-    const path = vis.groups
+      .attr('transform', `translate(${[vis.width, vis.height].map(d => d / 2)})`)
       .selectAll('path')
-      .data(datum.descendants().slice(1))
-      .join('path')
-
-      .attr('fill', d => {
-        while (d.depth > 1) d = d.parent
-        return color(d.data.name)
+      .data(datum)
+      .enter()
+      .append('path')
+      .attrs((d, i) => {
+        return {
+          d: vis.arc(d),
+          stroke: vis.color(d.data.label),
+          fill: vis.color(d.data.label),
+          'fill-opacity': (((1 - d.depth / 5) * 10) | 0) / 10
+        }
       })
-      .attr(
-        'fill-opacity',
-        d => 0.5
-        // vis.arcVisible(d.current) ? (d.children ? 0.6 : 0.4) : 0
-      )
-      .attr('d', d => vis.arc(d.current))
-
-    // console.log(path)
   }
 }
 
-// const sunburst = new Sunburst(data)
-
-function gen(depth) {
-  const children = []
-  let times = 0
-  const hasChild = d3.randomInt(0, 2)
-  while (times++ < 2) {
-    const obj = { value: 1, name: depth + '-' + times }
-    if (depth < 4) {
-      depth += 1
-      obj.children = gen(depth)
-    }
-    children.push(obj)
-  }
-
-  return children
-}
-
-// https://jsfiddle.net/b9a3y98s/1/
-//https://s.itho.me/modernweb/2015/slides/R2_0515_1330-1355_kuro%20hsu.pdf
-const randomData = { name: '1-1', value: 1, children: gen(1) }
-
-// console.log(JSON.stringify(randomData, null, 2))
-const width = 800
-const height = 600
-const view = d3
-  .select('#chart_svg2')
-  .append('svg')
-  .attrs({ width, height })
-  .append('g')
-  .attr('transform', 'translate(40,30)')
-const theme = d3.schemeYlGn
-
-const radius = d3.min([800, 600]) / 2
-const root = d3
-  .hierarchy(randomData)
-  .sum(d => d.value)
-  .sort((a, b) => b.value - a.value)
-const partition = d3.partition().size([800, 500])(root)
-const arc = d3
-  .arc()
-  .startAngle(d => d.x0)
-  .endAngle(d => d.x1)
-  .innerRadius(2)
-  .outerRadius(20)
-
-console.log(root)
-view
-  .selectAll('line')
-  .data(partition)
-  .enter()
-  .append('line')
-  .attrs((d, i) => {
-    console.log()
-    return {
-      x0: d.x1,
-      x1: d.x1,
-      y0: d.y1,
-      y1: d.y1,
-      stroke: theme[9][d.depth % 9]
-    }
-  })
-
-view
-  .selectAll('text')
-  .data(partition)
-  .enter()
-  .append('text')
-  .attr('x', d => d.x1 - 15)
-  .attr('y', d => d.y1 - 0)
-
-  .text(d => d.data.name)
-let px = 1
-for (const color of theme[9]) {
-  view
-    .append('text')
-    .attr('fill', color)
-    .attr('x', 600 - px * 10)
-    .text(px)
-  px++
-}
+const sunburst = new Sunburst(randomData)
